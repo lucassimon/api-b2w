4b42f2395c505c76cbb3cc6448ec51902c868f1f
Lucas Simon <lucassrod@gmail.com>
2019-07-22 17:39:02 -0300

    Feito o tratamento dos dados no schema
    Os dados agora estão no post_load em caixa baixa e sem espacos

--- apps/planets/schemas.py
+++ apps/planets/schemas.py
@@ -1,6 +1,6 @@
 
 
-from marshmallow import Schema
+from marshmallow import Schema, post_load
 from marshmallow.fields import Str
 
 # -*- coding: utf-8 -*-
@@ -8,15 +8,25 @@ from marshmallow.fields import Str
 # importar nossas CONSTANTES
 from apps.messages import MSG_FIELD_REQUIRED
 
+
 class PlanetRegistrationSchema(Schema):
-    name = Str(required=True, error_messages={MSG_FIELD_REQUIRED})
+    name = Str(required=True, error_messages={"required": MSG_FIELD_REQUIRED})
     climate = Str()
     terrain = Str()
+
+    @post_load
+    def lowerstrip(self, item, **kwargs):
+        item["name"] = item["name"].lower().strip()
+
+        if "climante" in item:
+            item["climate"] = item["climate"].lower().strip()
+
+        if "terrain" in item:
+            item["terrain"] = item["terrain"].lower().strip()
+        return item
 
 
 class PlanetSchema(Schema):
-    name = Str(required=True, error_messages={MSG_FIELD_REQUIRED})
+    name = Str(required=True, error_messages={"required": MSG_FIELD_REQUIRED})
     climate = Str()
     terrain = Str()
-    # cpf_cnpj = Str()
-    # active = Boolean()
